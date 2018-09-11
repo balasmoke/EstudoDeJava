2255092b9f2034546d5b1869dc37454ecff3e5cf
Gabriel de Moura Silva <gm.silva111297@gmail.com>
2018-09-11 11:07:00 -0300

    3.3 - Variaveis Tipo Var - Conclúido

--- 3.3 - Trabalhando com tipo Var/src/Main.java
+++ 3.3 - Trabalhando com tipo Var/src/Main.java
@@ -1,7 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        
+        //tipos var são alteraveis é e podem mudar sempre diferente das outras
+        //tipo var esta disponivel apartir do Java 10
+
+        int x = 10;
+        var x2 = 20;
+        x2 = 10.0;
+
+        boolean y = false;
+        var y2 = true;
+
+        var z;
 
     }
 }
